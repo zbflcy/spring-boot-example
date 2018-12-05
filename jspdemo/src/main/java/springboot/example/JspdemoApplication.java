fe59849b09005ee27aaf230b7d330fd2833665db
herewithyou <1274108397@qq.com>
2018-12-05 17:01:59 +0800

    jspdemo

---
+++ jspdemo/src/main/java/springboot/example/JspdemoApplication.java
@@ -0,0 +1,12 @@
+package springboot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspdemoApplication.class, args);
+	}
+}
